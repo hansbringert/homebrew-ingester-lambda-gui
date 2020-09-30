ddbc62cb6de8ea047e3f7be0286b90e4977636aa
Hans Bringert <hans.bringert@infomaker.se>
2020-09-30 13:34:47 +0200

    version v.0.1

---
+++ ingester-lambda-gui.rb
@@ -0,0 +1,27 @@
+# comment
+# frozen_string_literal: true
+
+# top-level class comment
+class IngesterLambdaGui < Formula
+  desc "Ingester Lambda GUI"
+  homepage "https://bitbucket.org/infomaker/"
+  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/ingester-lambda-gui/ingester-lambda-gui-v.0.1.tar.gz"
+  sha256 "c57c07ff685f82cdf93d22b3ebe8c90bf8c127ca6eb290c8ad0817d753710edf"
+
+  def install
+    bin.install "ingester-lambda-gui"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test oc-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "#{bin}/ingester-lambda-gui"
+  end
+end

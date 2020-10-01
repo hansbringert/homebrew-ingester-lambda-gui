e861cf1ecb2b05bd972768303bc45793bc26c95a
Hans Bringert <hans.bringert@infomaker.se>
2020-10-01 15:02:15 +0200

    version 9abb313

--- ingester-lambda-gui.rb
+++ ingester-lambda-gui.rb
@@ -5,8 +5,8 @@
 class IngesterLambdaGui < Formula
   desc "Ingester Lambda GUI"
   homepage "https://bitbucket.org/infomaker/"
-  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/ingester-lambda-gui/ingester-lambda-gui-9abb3131eb516eec2cb16bdf1cf13423c4c0f042.tar.gz"
-  sha256 "db7ac99c1c0b7c775c1f95211aadf5b3acde8b84399c13f39a66beb792e5689d"
+  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/ingester-lambda-gui/ingester-lambda-gui-9abb313.tar.gz"
+  sha256 "45ff0f45b60d328aa3b7b234eae48286d70588c61dbb4c5dcefa095d760ffb97"
 
   def install
     bin.install "ingester-lambda-gui"
